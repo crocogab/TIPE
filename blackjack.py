8e0220a0be5fbd27fba40dd3f61428132afbf422
Syycorax <54723548+Syycorax@users.noreply.github.com>
2023-02-09 10:04:21 +0100

    docs: added docstrings

--- blackjack.py
+++ blackjack.py
@@ -32,6 +32,9 @@ class hand:
         pass
 
     def get_value(self) -> int:
+        """
+        Fonction qui retourne la valeur de la main
+        """
         total = 0
         for card in self.l_cards:
             total += card.value if card.value < 10 else 10
@@ -46,24 +49,75 @@ class Deck:
         return f"Nombre de cartes [PILE] : {self.nb_cards} , cartes = {self.l_cards}"
 
     def swap(self, i: int, j: int):
+        """
+        Fonction qui permute deux cartes dans le deck
+        """
         temp = self.l_cards[j]
         self.l_cards[j] = self.l_cards[i]
         self.l_cards[i] = temp
 
-    def shuffle(self):
-        for i in range(self.nb_cards):
-            self.swap(i, rd.randint(0, self.nb_cards-1))
+    def shuffle(self, n: int):
+        """
+        Mélange un deck n fois
+        TODO: implementer un algo de mélangage plus réaliste
+        """
+        for i in range(n):
+            for j in range(self.nb_cards):
+                self.swap(j, rd.randint(0, self.nb_cards-1))
+
+    def draw(self) -> Card:
+        """
+        Retourne la carte du dessus du deck et la supprime du deck
+        """
+        card = self.l_cards[0]
+        self.l_cards.pop(0)
+        self.nb_cards -= 1
+        return card
 
 
 class Player:
+    """
+    Classe du joueur
+    choix: true = prend une carte, false = rester
+    is_out: true = le joueur est out(+21), false = le joueur est en jeu
+    stopped: true = le joueur a arreté de tirer des cartes, false = le joueur peut encore tirer des cartes
+    """
     def __init__(self, hand: hand, id: int):
         self.id = id
         self.hand = hand
         self.is_out = False
+        self.stopped = False
 
     def check(self):
+        """
+        Fonction qui verifie si le joueur est out
+        """
         if self.hand.get_value() > 21:
             self.is_out = True
+
+    def play(self):
+        """
+        Fonction qui permet au joueur de jouer
+        """
+        # TODO: implémenter le choix
+        choice = True
+        if choice and not self.stopped:
+            Game.give_card(self)
+        else:
+            self.stopped = True
+
+
+class Game:
+    def __init__(self, players: list, deck: Deck):
+        self.players = players
+        self.deck = deck
+
+    def give_card(self, player: Player):
+        """
+        Fonction qui donne une carte au joueur
+        """
+        player.hand.l_cards.append(self.deck.draw())
+        player.hand.nb_cards += 1
 
 
 def make_deck(n: int) -> Deck:
