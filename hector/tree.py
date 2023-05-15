2e3359dd881d8e42df89f2cd9aa59b9e0601f194
Syycorax <54723548+Syycorax@users.noreply.github.com>
2023-05-15 15:04:00 +0200

    lesgongue

--- hector/tree.py
+++ hector/tree.py
@@ -1,19 +1,27 @@
+import time
+
 SAFENESS = 0.5
 # SAFENESS represents the minimum survival rate of a node's children to be considered a good node
+# the rate is calculated by dividing the number of children with a value < 21 by the total number of children
 
 
 class Tree():
-    def __init__(self, value: int, children=None):
+    def __init__(self, value: int, total: int, children=None):
         self.val = value
         self.children = children or []
-        self.parent = None
-        for child in self.children:
-            child.parent = self
+        self.total = self.val + total
+        # self.parent = None            parents should not be needed
+        # for child in self.children:
+        #     child.parent = self
+
+    def survival_meth(self):
         try:
             self.survival = len(
-                [child for child in self.children if child.val < 21]) / len(self.children)
+                [child for child in self.children if child.total < 21]) / len(self.children)  # there is a bug here. Children val will always be less than 21
         except ZeroDivisionError:
             self.survival = 0
+        for child in self.children:
+            child.survival_meth()
 
     def is_empty(self):
         return self.val == -1
@@ -31,25 +39,43 @@ class Tree():
         return self.survival > SAFENESS
 
 
-def list_to_tree(l: list):
-    """transforme une liste de liste en arbre
-    [0[1[2]][2][3][4][5][6]] cette liste renverra
-    Tree(0, [Tree(1, [Tree(2)]), Tree(2), Tree(3), Tree(4), Tree(5), Tree(6)])"""
-    if len(l) == 0:
-        return Tree(-1)
-    if len(l) == 1:
-        return Tree(l[0])
-    return Tree(l[0], [list_to_tree(l[1:])])
+def create_game_tree(current: Tree, depth: int):
+    """creates a game tree with a maximum depth of 6"""
+    cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    maxdepth = 6
+    if depth == maxdepth:
+        return current
+    else:
+        for card in cards:
+            current.children.append(Tree(card, current.total))
+            # si la main dépasse 21, on n'a pas besoin de créer les enfants car ils sont tous perdants
+    for child in current.children:
+        create_game_tree(child, depth + 1)
+    return current
 
 
-MAINTREE = Tree(-1)
+    # MAINTREE est un arbre vide, il sera rempli par game_tree
+MAINTREE = Tree(-1, 0)
 
 
 def main():
-    next = input("Enteer la carte recue : ")
-    MAINTREE.navigate(int(next))
-    print("Voici ce que vous devez faire :", +
-          "prendre" if MAINTREE.shouldtake()else "ne pas prendre")
+    global MAINTREE
+    timestamp = time.time()
+    mytree = create_game_tree(MAINTREE, 0)
+    mytree.survival_meth()
+    print(time.time() - timestamp)
+    while True:
+        next = input("Entrer la carte recue : ")
+        while not next.isdigit() or int(next) not in range(1, 11):
+            next = input("Entrée incorrecte : ")
+        mytree = mytree.navigate(int(next))
+        print([child.val for child in mytree.children])
+        if mytree.shouldtake():
+            print("Je prends")
+            print(mytree.survival)
+        else:
+            print("Je ne prends pas")
+            print(mytree.survival)
 
 
 main()
