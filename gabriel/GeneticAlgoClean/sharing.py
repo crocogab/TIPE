3a9e3dcf5c9200647d2bfa33a16cb19fbc1e02f9
crocogab <50633541+crocogab@users.noreply.github.com>
2023-10-03 15:55:47 +0200

    Update sharing.py

--- gabriel/GeneticAlgoClean/sharing.py
+++ gabriel/GeneticAlgoClean/sharing.py
@@ -3,73 +3,93 @@ from math import sqrt
 
 #inutile pour les petites populations
 
-d_min=0.1
-d_max=0.5
+d_min=4
+d_max=2
 alpha=0.8
 
 class cluster:
     def __init__(self) -> None:
         self.individus = []
-        self.centre = None
+        self.centre = []
 
 
-def distance(i1: Individu, i2: Individu):
-    """Distance entre deux individus"""
-    return sqrt(sum([(i1.chromosomes[i] - i2.chromosomes[i])**2 for i in range(190)]))
+def distance_individus(i1: Individu, i2: Individu):
+  """Distance entre deux individus"""
+  return sqrt(sum([(i1.chromosomes[i] - i2.chromosomes[i])**2 for i in range(190)]))
+
+def distance_liste(l1:list,l2:list):
+  """Distance entre deux listes (de chromosomes)"""
+  return sqrt(sum([(l1[i] - l2[i])**2 for i in range(190)]))
 
 
-    
 def init_clusters(individus: list):
-    """Initialise les clusters avec un individu chacun"""
-    clusters = []
-    indiviu_zero=Individu()
-    for i in individus:
-        clusters.append(cluster())
-        clusters[-1].individus.append(i)
-        clusters[-1].centre = distance(i, indiviu_zero)
-    return clusters    
+  """Initialise les clusters avec un individu chacun"""
+  clusters = []
+  for i in individus:
+      clusters.append(cluster())
+      clusters[-1].individus.append(i)
+      clusters[-1].centre = i.chromosomes
+  print("[DEBUG] initialisation terminee")
+  return clusters    
 
-def update_clusters(clusters: list):
-    """Met à jour les centres des clusters"""
-    for c in clusters:
-        c_clusters = clusters[:]
-        c_clusters.remove(c)
-        for o in c_clusters:
-            if abs(c.centre - o.centre) < d_min:
-                c.individus.extend(o.individus)
-                c.centre = (c.centre + o.centre)/2
-        if len(c.individus) ==1:
-            min=abs(c.centre-c_clusters[0].centre)
-            temp_c=c_clusters[0]
-            for o in c_clusters:
-                if abs(c.centre - o.centre) < min:
-                    min=abs(c.centre-o.centre)
-                    temp_c=o
-            if min < d_max:
-                temp_c.individus.append(c.individus[0])
-                temp_c.centre = ((temp_c.centre)*len(temp_c.individus) + c.centre)/len(temp_c.individus)
+def fusion_clusters(clusters: list):
+  """Met à jour les centres des clusters 
+  ATTENTION -> ne conserve pas forcement la taille de la liste clusters
+  """
+  liste_a_del=[]
+  for i in range(len(clusters)):
+    for j in range(len(clusters)):
+      if i!=j:
+        if distance_liste(clusters[i].centre,clusters[j].centre)<d_min:
+          print(f"[DEBUG] fusion entre {i} {j}")
+          clusters[i].centre=[(clusters[i].centre[l]+clusters[j].centre[l])/2 for l in range(190)]
+          clusters[i].individus.extend(clusters[j].individus)
+          liste_a_del.append(j)
+  a=0
+  for j in liste_a_del:
+    del clusters[j-a]
+    a+=1 
+ 
+  
+
+def add_individu(i1:Individu,clusters:list):
+  """Ajoute l'individu à la liste des clusters -> crée les nouveaux clusters
+  Ou modifie barycentre
+  """
+  etat=0
+  indice=-1
+  for indice_c in range(len(clusters)):
+    if distance_liste(i1.chromosomes,clusters[indice_c].centre)<d_max:
+      etat=1
+      indice=indice_c
+  if etat==1:
+    clusters[indice].centre=[(clusters[indice].centre[l]+i1.chromosomes[l])/2 for l in range(190)]
+  else:
+    clusters.append(cluster(individus=i1,centre=i1.chromosomes))
+
+
+
+
+
+
 
 def individu_clusters(i1: Individu, clusters: list):
     """Nombre d'individus dans le cluster de i1"""
     for c in clusters:
         if i1 in c.individus:
             return (len(c.individus),c)
-    print( "Error individu not found") # cochon -> debug
+  
+    print( f"Error individu not found {type(c)} {i1}") # cochon -> debug
     
 
 
-def sharing(individu_list):
-    sharing_val= []
-    clusters=init_clusters(individu_list)
-    update_clusters(clusters)
-    indiviu_zero=Individu()
-    for individu in individu_list:
-        nb_individu,cluster=individu_clusters(individu,clusters)
-        d_ic=abs(distance(individu,indiviu_zero)-cluster.centre)
-        mi= nb_individu * (1-(d_ic/2*d_max)**alpha)
-        sharing_val.append(mi)
+def sharing(i1:Individu,clusters:list):
+  """ Renvoie la valeur du sharing pour un individu """
+  
+  nc,c=individu_clusters(i1,clusters)
+  return nc*(1-((distance_liste(i1.chromosomes,c.centre))/(2*d_max))**alpha)
 
-    return sharing_val
+    
 
                 
                 
